1224f53aaee2b31e645b2a811d8be1b7e63c2a46
Leoberto Soares <leoberto@wisecare.tech>
2021-04-26 11:28:42 -0300

    add example folder

---
+++ example/v4h.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+$(document).ready(() => {
+  console.log(WiseAPI);
+
+  const v4hapi = new WiseAPI();
+
+  v4hapi.setup({
+    baseUrl: 'https://session-manager.homolog.v4h.cloud/api',
+    domain: 'conf.homolog.v4h.cloud',
+  }).then(async () => {
+
+    const session = await v4hapi.session.create({
+      profile: 'DEFAULT',
+      skin: 'DEFAULT',
+      org: 'organizacao', // change this
+      orgUnit: 'geral', // change this
+      joinPolicy: 'PUBLIC',
+      listPolicy: 'PUBLIC',
+    });
+
+    v4hapi.session.startConference(session.short, {
+      parentNode: document.getElementById('meet'),
+      startWithAudioMuted: true,
+      userInfo: { displayName: 'Leoberto' },
+      shareLink: 'https://v4h.cloud/teste',
+      buttons: ['microphone', 'camera', 'closedcaptions', 'desktop', 'fullscreen',
+        'fodeviceselection', 'hangup', 'profile', 'info', 'chat', 'recording',
+        'livestreaming', 'etherpad', 'sharedvideo', 'settings', 'raisehand',
+        'videoquality', 'filmstrip', 'invite', 'feedback'],
+    });
+
+  });
+});
